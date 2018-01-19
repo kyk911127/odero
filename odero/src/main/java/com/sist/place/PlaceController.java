f701dc32c3698d42cc3f69adb155a7a115236148
yena <yena@SIST215>
2018-01-19 19:45:33 +0900

    p_detail수정

--- odero/src/main/java/com/sist/place/PlaceController.java
+++ odero/src/main/java/com/sist/place/PlaceController.java
@@ -1,6 +1,10 @@
 package com.sist.place;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +15,12 @@ public class PlaceController {
 	}
 	
 	@RequestMapping("p_detail.do")
-	public String place_detail() {
+	public String place_detail(Model model) {
+		
+		String[] timg={"p_image/top.jpg","p_image/top1.jpg",  "p_image/top2.jpg","p_image/top3.jpg"};
+		
+		model.addAttribute("timg",timg);
+		
 		return "place/p_detail";
 	}
 }
