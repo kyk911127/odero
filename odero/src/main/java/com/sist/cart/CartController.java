dbce378ceeb9eeae0914ecbdac05038ef3a61d4f
KOO <foresg@gmail.cm>
2018-01-19 16:20:13 +0900

    mypage 세분화 (승구)

--- odero/src/main/java/com/sist/cart/CartController.java
+++ odero/src/main/java/com/sist/cart/CartController.java
@@ -4,7 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CartController {
 	@RequestMapping("mypage.do")
-	public String main_page() {
+	public String mypage() {
 		return "cart/mypage";
+	}
+	@RequestMapping("mypage_list.do")
+	public String mypage_list() {
+		return "cart/mypage_list";
+	}
+	@RequestMapping("mypage_cos.do")
+	public String mypage_cos() {
+		return "cart/mypage_cos";
+	}
+	@RequestMapping("mypage_place.do")
+	public String mypage_place() {
+		return "cart/mypage_place";
 	}
 }
