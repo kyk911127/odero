eafed40184580b1764c090271bd132c52ab26a19
Seung9 <Seung9@KOO-vivobooks>
2018-01-22 16:39:13 +0900

    카트VO 추가/변경

---
+++ odero/src/main/java/com/sist/cart/dao/CartPlaceVO.java
@@ -0,0 +1,76 @@
+package com.sist.cart.dao;
+
+public class CartPlaceVO {
+	private int p_no;
+	private String p_name;
+	private String p_addr;
+	private String p_price;
+	private String p_keyword;
+	private String p_img;
+	private String p_tel;
+	private String p_grade;
+	private String p_time;
+	private String p_hit;
+	
+	public int getP_no() {
+		return p_no;
+	}
+	public void setP_no(int p_no) {
+		this.p_no = p_no;
+	}
+	public String getP_name() {
+		return p_name;
+	}
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+	public String getP_addr() {
+		return p_addr;
+	}
+	public void setP_addr(String p_addr) {
+		this.p_addr = p_addr;
+	}
+	public String getP_price() {
+		return p_price;
+	}
+	public void setP_price(String p_price) {
+		this.p_price = p_price;
+	}
+	public String getP_keyword() {
+		return p_keyword;
+	}
+	public void setP_keyword(String p_keyword) {
+		this.p_keyword = p_keyword;
+	}
+	public String getP_img() {
+		return p_img;
+	}
+	public void setP_img(String p_img) {
+		this.p_img = p_img;
+	}
+	public String getP_tel() {
+		return p_tel;
+	}
+	public void setP_tel(String p_tel) {
+		this.p_tel = p_tel;
+	}
+	public String getP_grade() {
+		return p_grade;
+	}
+	public void setP_grade(String p_grade) {
+		this.p_grade = p_grade;
+	}
+	public String getP_time() {
+		return p_time;
+	}
+	public void setP_time(String p_time) {
+		this.p_time = p_time;
+	}
+	public String getP_hit() {
+		return p_hit;
+	}
+	public void setP_hit(String p_hit) {
+		this.p_hit = p_hit;
+	}
+	
+}
