86f4a26f77c3dfdfe39b625292ac7b3d00b778bf
DS <DS@SIST233>
2018-01-22 16:35:45 +0900

    ds placeVO 커밋

--- odero/src/main/java/com/sist/main/dao/MainMapper.java
+++ odero/src/main/java/com/sist/main/dao/MainMapper.java
@@ -1,5 +1,11 @@
 package com.sist.main.dao;
 
-public interface MainMapper {
+import org.apache.ibatis.annotations.Select;
 
+public interface MainMapper {
+	//BEST FOOD
+/*	@Select("SELECT p_img, p_name, p_addr, p_no "
+			+ "FROM place "
+			+ "WHERE p_grade='f' ")
+	public PlaceVO best_food();*/
 }
