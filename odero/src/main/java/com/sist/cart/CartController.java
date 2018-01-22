c82557886b57eedb4d385329d87c9358f405b242
Seung9 <Seung9@KOO-vivobooks>
2018-01-22 21:10:22 +0900

    찜/마이페이지 DAO추가요

--- odero/src/main/java/com/sist/cart/CartController.java
+++ odero/src/main/java/com/sist/cart/CartController.java
@@ -1,10 +1,21 @@
 package com.sist.cart;
+import java.util.List;
+import com.sist.cart.dao.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
 @Controller
 public class CartController {
+	@Autowired
+	CartDAO dao;
 	@RequestMapping("mypage.do")
-	public String mypage() {
+	public String mypage(Model model) {
+		List<CartPlaceVO> list = dao.CartPlaceBest5();
+		model.addAttribute("list",list);
 		return "cart/mypage";
 	}
 	@RequestMapping("mypage_list.do")
