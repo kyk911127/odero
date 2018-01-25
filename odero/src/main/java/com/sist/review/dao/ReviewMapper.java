2ad436aed62bd829826ce754f092fdd1f05ee558
sist <sist@ns4>
2018-01-25 20:00:09 +0900

    09

--- odero/src/main/java/com/sist/review/dao/ReviewMapper.java
+++ odero/src/main/java/com/sist/review/dao/ReviewMapper.java
@@ -8,20 +8,20 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectKey;
 /*
  * private int r_no;
-	private String m_id;     ���̵�
-	private String r_subject;����
-	private String r_pname;  ���Ը�
-	private String r_content;����
-	private String r_hit;    ��ȸ��
-	private Date r_reddate;  ��ϳ�¥
-	private String r_imgname;�̹����̸�
-	private int r_imgcount;  �̹�������
+	private String m_id;     
+	private String r_subject;
+	private String r_pname;  
+	private String r_content;
+	private String r_hit;    
+	private Date r_reddate;  
+	private String r_imgname;
+	private int r_imgcount;  
  */
 public interface ReviewMapper {
 	//List
-	@Select("SELECT r_no,m_id,r_subject,r_pname,r_hit,r_regdate,r_imgname,num "
-		  + "FROM (SELECT r_no,m_id,r_subject,r_pname,r_hit,r_regdate,r_imgname,rownum AS num "
-		  + "FROM (SELECT r_no,m_id,r_subject,r_pname,r_hit,r_regdate,r_imgname "
+	@Select("SELECT r_no,m_id,r_subject,r_pname,r_addr,r_hit,r_regdate,r_imgname,num "
+		  + "FROM (SELECT r_no,m_id,r_subject,r_pname,r_addr,r_hit,r_regdate,r_imgname,rownum AS num "
+		  + "FROM (SELECT r_no,m_id,r_subject,r_pname,r_addr,r_hit,r_regdate,r_imgname "
 		  + "FROM review ORDER BY r_no DESC)) "
 		  + "WHERE num BETWEEN #{start} AND #{end}")
 	public List<ReviewVo> reviewList(Map map);
@@ -33,7 +33,7 @@ public interface ReviewMapper {
 	//Insert
 	@SelectKey(keyProperty="r_no", resultType=int.class, before=true, 
 			statement="SELECT NVL(MAX(r_no)+1,1) AS no FROM review")
-	@Insert("INSERT INTO review VALUES(r_no,#{m_id},#{r_subject},#{r_pname},#{r_content},0,SYSDATE,"
+	@Insert("INSERT INTO review VALUES(#{r_no},#{m_id},#{r_subject},#{r_pname},#{r_content},0,SYSDATE,"
 		  + "#{r_imgname},#{r_imgcount},#{r_addr})")
 	public void reviewInsert(ReviewVo vo);
 }
