a0de4e13fdfe51af76d57763bb07adef90c66b5e
sist <sist@ns3>
2018-01-30 19:04:28 +0900

    yeseul commit

--- odero/src/main/java/com/sist/course/CourseController.java
+++ odero/src/main/java/com/sist/course/CourseController.java
@@ -14,11 +14,12 @@ import com.sist.place.dao.PlaceVO;
 public class CourseController {
 	@Autowired
 	private CourseDAO dao;
-	
+
 	@RequestMapping("course_search.do")
 	public String main_page(String strPosition, String strPno, Model model) {
 		String[] strPositions = new String[9];
 		String[] strPnos = new String[9];
+		List<PlaceVO> p_list = new ArrayList<PlaceVO>();
 		System.out.println("strPosition(controller) : " + strPosition);
 		System.out.println("strPno(controller) : " + strPno);
 		if (strPosition == null || strPno == null) {
@@ -27,7 +28,7 @@ public class CourseController {
 		} else {
 			strPosition = strPosition.substring(0, strPosition.lastIndexOf("_"));
 			strPno = strPno.substring(0, strPno.lastIndexOf("_"));
-			
+
 			String[] pc_list = strPosition.split("_");
 			String[] pno_list = strPno.split("_");
 			for (int i = 0; i < 9; i++) {
@@ -36,9 +37,17 @@ public class CourseController {
 				strPnos[i] = pno_list[i];
 				System.out.println(strPositions[i]);
 			}
-			
+
+			// pno_list에 있는 가게번호로 가게 정보 넘기기
+			for (String no : pno_list) {
+				PlaceVO vo = dao.course_place_data(Integer.parseInt(no));
+				p_list.add(vo);
+			}
+
 			System.out.println("strPosition2(controller) : " + strPosition);
 		}
+
+		model.addAttribute("p_list", p_list);
 		model.addAttribute("strPosition", strPositions);
 		model.addAttribute("strPnos", strPnos);
 		return "course/course_search";
@@ -47,32 +56,42 @@ public class CourseController {
 	// 검색버튼 눌렀을 때
 
 	@RequestMapping("course_search_ok.do")
-  public String course_search_ok(Model model) {
-   //guList : 선택된 구 리스트 , keyList : 선택된 키워드 리스트 
-   //         keyList[0] : food, [1] : play, [2] : cafe
-   //임의의 데이터 9개
-   int[] no_list = {4307,4299,4320,4483,4515,4599,4553,4577,4610};
-   
-   List<PlaceVO> p_list = new ArrayList<PlaceVO>();
-   for(int i=0; i<9; i++) {
-	   PlaceVO vo = dao.course_place_data(no_list[i]);
-	   System.out.println("======================");
-	   System.out.println("가게번호 : " + vo.getP_no());
-	   System.out.println("가게이름 : " + vo.getP_name());
-	   System.out.println("가게유형 : " + vo.getP_grade());
-	   System.out.println("가게주소 : " + vo.getP_addr());
-	   System.out.println("조회수 : " + vo.getP_hit());
-	   System.out.println("======================");
-	   p_list.add(vo);
-   }
-   //랜덤으로 맛집 3, 놀거리 3, 카페 3 뽑기
-   
-   //랜덤으로 뽑힌 가게 번호 리스트
-   //List<Integer> p_noList = new ArrayList<Integer>();
+	public String course_search_ok(Model model) {
+		// guList : 선택된 구 리스트 , keyList : 선택된 키워드 리스트
+		// keyList[0] : food, [1] : play, [2] : cafe
+		// 임의의 데이터 9개
+		int[] no_list = { 4307, 4299, 4320, 4483, 4515, 4599, 4553, 4577, 4610 };
 
-   model.addAttribute("p_list", p_list);
-   return "course/course_search_ok";
+		List<PlaceVO> p_list = new ArrayList<PlaceVO>();
+		for (int i = 0; i < 9; i++) {
+			PlaceVO vo = dao.course_place_data(no_list[i]);
+			System.out.println("======================");
+			System.out.println("가게번호 : " + vo.getP_no());
+			System.out.println("가게이름 : " + vo.getP_name());
+			System.out.println("가게유형 : " + vo.getP_grade());
+			System.out.println("가게주소 : " + vo.getP_addr());
+			System.out.println("조회수 : " + vo.getP_hit());
+			System.out.println("======================");
+			p_list.add(vo);
+		}
+		// 랜덤으로 맛집 3, 놀거리 3, 카페 3 뽑기
 
-  }
+		// 랜덤으로 뽑힌 가게 번호 리스트
+		// List<Integer> p_noList = new ArrayList<Integer>();
+
+		model.addAttribute("p_list", p_list);
+		return "course/course_search_ok";
+
+	}
+	
+	@RequestMapping("placetoplace.do")
+	public String place_to_place(int p_no, int cnt, Model model) {
+		PlaceVO vo = dao.course_place_data(p_no);
+		System.out.println("p_no : " + p_no);
+		System.out.println("cnt : " + cnt);
+		System.out.println("vo.getP_name() : " + vo.getP_name());
+		model.addAttribute("vo", vo);
+		return "course/place_view/place_" + cnt;
+	}
 
 }
---
+++ odero/src/main/webapp/WEB-INF/course/place_view/place_3.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+
+</head>
+<body>
+	<table class="tmap_wrap">
+		<tr>
+			<td style="padding: 3%;">
+				<h4 style="float: left;">
+					<scan class="glyphicon glyphicon-home" style="margin-right:10px;">${vo.p_name }
+					
+				</h4>
+			</td>
+		</tr>
+		<tr>
+			<td><span class="tmap_detail">&nbsp;${vo.p_addr }</span></td>
+		</tr>
+		<tr>
+			<td class="text-right">
+			<span class="tmap_detail glyphicon glyphicon-eye-open">${vo.p_hit }</span>
+			<span class="tmap_detail glyphicon glyphicon-heart">찜&nbsp;</span></td>
+		</tr>
+	</table>
+</body>
+</html>
