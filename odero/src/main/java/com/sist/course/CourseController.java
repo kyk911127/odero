c599ddda01f400e78352d2f3f6e0b2e22a400b99
sist <sist@ns2>
2018-01-31 11:25:24 +0900

    ㅋㅁ

--- odero/src/main/java/com/sist/course/CourseController.java
+++ odero/src/main/java/com/sist/course/CourseController.java
@@ -93,5 +93,72 @@ public class CourseController {
 		model.addAttribute("vo", vo);
 		return "course/place_view/place_" + cnt;
 	}
+	
+	
+	
+	
+	@RequestMapping("course_search.do")
+    public String course_search(Model model){
+        
+        
+        String[] gu={"은평구","마포구","서대문","종로구","용산구",
+                "중구","성동구","동대문구","광진구","중랑구",
+                "성북구","노원구","도봉구","중구","강서구",
+                "양천구","영등포구","구로구","동작구","금천구",
+                "관악구","서초구","강남구","송파구","강동구"};
+        
+        List<PlaceVO> gu_list= new ArrayList<PlaceVO>();
+        /*for(int i=0; i<gu.length; i++){
+            gu_list.add(gu[i]);
+        }*/
+        
+        
+        //키워드 split하기
+        List<PlaceVO> fk_list=dao.course_pkeyword();
+        for(PlaceVO vo:fk_list){
+            String fk=vo.getP_keyword();
+            String[] arrPkey=fk.split("[,]");
+            /*if(fk.equals(" ")){
+                
+            }else{
+                String[] arrPkey=fk.split("[,]");
+                //여기서 splay테이블로 자른 키워드를 insert하냐~~~~~?
+            }*/
+        }
+        
+        List<PlaceVO> pk_list=dao.course_pkeyword();
+        for(PlaceVO vo:pk_list){
+            String pk=vo.getP_keyword();
+            String[] arrPkey=pk.split("[,]");
+            /*if(pk.equals(" ")){
+                
+            }else{
+                String[] arrPkey=pk.split("[,]");
+                //여기서 splay테이블로 자른 키워드를 insert하냐~~~~~?
+            }*/
+        }
+        
+        
+        List<PlaceVO> ck_list=dao.course_pkeyword();
+        for(PlaceVO vo:ck_list){
+            String ck=vo.getP_keyword();
+            String[] sp_key=ck.split("[,]");
+            //여기서 splay테이블로 자른 키워드를 insert하냐~~~~~?
+            /*if(ck.equals(" ")){
+                
+            }else{
+                String[] arrPkey=ck.split("[,]");
+                //여기서 splay테이블로 자른 키워드를 insert하냐~~~~~?
+            }*/
+        }
+        
+        
+        model.addAttribute("fk_list",pk_list);
+        model.addAttribute("pk_list",pk_list);
+        model.addAttribute("ck_list",pk_list);
+        
+        return "course/course_search";
+    }
+
 
 }
--- odero/src/main/java/com/sist/course/dao/CourseDAO.java
+++ odero/src/main/java/com/sist/course/dao/CourseDAO.java
@@ -1,5 +1,9 @@
 package com.sist.course.dao;
 
+import java.util.List;
+
+import javax.ws.rs.DELETE;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -7,10 +11,85 @@ import com.sist.place.dao.PlaceVO;
 
 @Repository
 public class CourseDAO {
-	@Autowired
-	private CourseMapper mapper;
-	
-	public PlaceVO course_place_data(int no) {
-		return mapper.course_place_data(no);
-	}
+    @Autowired
+    private CourseMapper mapper;
+    
+    public PlaceVO course_place_data(int no) {
+        return mapper.course_place_data(no);
+    }
+    
+    
+    //지역
+    public List<PlaceVO> course_gu(){
+        return mapper.course_gu();
+    }
+    
+    
+    //place-키워드
+    
+    public List<PlaceVO> course_fkeyword(){
+        return mapper.course_fkeyword();
+    }
+    
+    
+    public List<PlaceVO> course_pkeyword(){
+        return mapper.course_pkeyword();
+    }
+    
+    
+    public List<PlaceVO> course_ckeyword(){
+        return mapper.course_ckeyword();
+    }
+    
+    
+    
+    
+    
+    //컬럼의 행 삭제
+    public void sfood_delete(){
+        mapper.sfood_delete();
+    }
+    
+    public void splay_delete(){
+        mapper.splay_delete();
+    }
+    
+    public void scafe_delete(){
+        mapper.scafe_delete();
+    }
+    
+    
+    
+    
+    //카테고리테이블에 각각 키워드 넣기
+    public List<SFoodVO> sfood_insert(){
+        return mapper.sfood_insert();
+    }
+    
+    public List<SPlayVO> splay_insert(){
+        return mapper.splay_insert();
+    }
+    
+    public List<SCafeVO> scafe_insert(){
+        return mapper.scafe_insert();
+    }
+    
+    
+    
+    public  List<SFoodVO> sfood_distinct(){
+        return mapper.sfood_distinct();
+    }
+    
+    public List<SPlayVO> splay_distinct(){
+        return mapper.splay_distinct();
+    }
+    
+    public List<SCafeVO> scafe_distinct(){
+        return mapper.scafe_distinct();
+    }
+    
+
+    
+
 }
+
