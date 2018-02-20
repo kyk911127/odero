17ec5449f289fc3f449060c4fba34af4cacb4961
sist <sist@ns7>
2018-02-20 17:43:38 +0900

    commit

--- odero/src/main/java/com/sist/place/PlaceController.java
+++ odero/src/main/java/com/sist/place/PlaceController.java
@@ -105,51 +105,5 @@ public class PlaceController {
 	}
 	
 	
-	/*@RequestMapping("s_list.do")
-	public String placeSelectData(String sn_1,String sn_3,String page, Model model){
-		
-		if(page==null)
-			page="1";
-		int curpage= Integer.parseInt(page);
-		int rowsize=8;
-		int start=(rowsize*curpage)-(rowsize-1);
-		int end= rowsize * curpage;
-		
-		System.out.println("page: "+page);
-		System.out.println("start: "+start);
-		System.out.println("end: "+end);
-		System.out.println("sn_1: "+sn_1);
-		
-		Map map=new HashMap();
-		map.put("sn_3", sn_3);
-		map.put("start", start);
-		map.put("end", end);
-		
-		model.addAttribute("curpage",curpage);
-		model.addAttribute("sn_1",sn_1);
-		
-		if(sn_1.equals("이색/체험"))
-		{
-			List<PlaceVO> sp_list = dao.placeSelectData_p(map);
-			int sp_totalpage=dao.placeTotalpage_sp(sn_3);
-			
-			model.addAttribute("sp_list",sp_list);
-			model.addAttribute("sp_totalpage",sp_totalpage);
-			
-		}
-		else  
-		{
-			List<PlaceVO> cf_list=dao.placeSelectData_cf(map);
-			int cf_totalpage=dao.placeTotalpage_cf(sn_3);
-			
-			model.addAttribute("cf_list",cf_list);
-			model.addAttribute("cf_totalpage",cf_totalpage);
-			
-		}
-		
-
-		return "place/list_view/s_list";
-	}
-	*/
 	
 }
